c3c5a0bab60c5c19f0d5f50a592f2af47f69e92f
masatonara <masato.nara83@gmail.com>
2021-05-27 10:16:07 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,51 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |n|
-    User.create!(
-      email: "test#{n + 1}@test.com",
-      name: "test#{n + 1}",
-      password: "test01",
-      user_image: File.open('./app/assets/images/test1.jpg')
-    )
-end
-
-5.times do |n|
-    User.create!(
-      email: "men#{n + 1}@test.com",
-      name: "men#{n + 1}",
-      password: "test01",
-      user_image: File.open('./app/assets/images/test2.jpg')
-    )
-end
-
-5.times do |n|
-    User.create!(
-      email: "women#{n + 1}@test.com",
-      name: "women#{n + 1}",
-      password: "test01",
-      user_image: File.open('./app/assets/images/test3.jpg')
-    )
-end
-
-5.times do |n|
-    User.create!(
-      email: "#jin#{n + 1}@test.com",
-      name: "#jin#{n + 1}",
-      password: "test01",
-      user_image: File.open('./app/assets/images/test4.jpg')
-    )
-end
-
-5.times do |n|
-    User.create!(
-      email: "beer#{n + 1}@test.com",
-      name: "beer#{n + 1}",
-      password: "test01",
-      user_image: File.open('./app/assets/images/test5.jpg')
-    )
-end
-
 Genre.create!(
     genre_name: "ビール",
   )
@@ -120,6 +75,50 @@ Genre.create!(
   )
 
 
+5.times do |n|
+    User.create!(
+      email: "test#{n + 1}@test.com",
+      name: "test#{n + 1}",
+      password: "test01",
+      user_image: File.open('./app/assets/images/test1.jpg')
+    )
+end
+
+5.times do |n|
+    User.create!(
+      email: "men#{n + 1}@test.com",
+      name: "men#{n + 1}",
+      password: "test01",
+      user_image: File.open('./app/assets/images/test2.jpg')
+    )
+end
+
+5.times do |n|
+    User.create!(
+      email: "women#{n + 1}@test.com",
+      name: "women#{n + 1}",
+      password: "test01",
+      user_image: File.open('./app/assets/images/test3.jpg')
+    )
+end
+
+5.times do |n|
+    User.create!(
+      email: "#jin#{n + 1}@test.com",
+      name: "#jin#{n + 1}",
+      password: "test01",
+      user_image: File.open('./app/assets/images/test4.jpg')
+    )
+end
+
+5.times do |n|
+    User.create!(
+      email: "beer#{n + 1}@test.com",
+      name: "beer#{n + 1}",
+      password: "test01",
+      user_image: File.open('./app/assets/images/test5.jpg')
+    )
+end
 
 10.times do |n|
     Liquor.create!(
@@ -134,5 +133,4 @@ Genre.create!(
       rate: "5"
     )
   end
-
 
