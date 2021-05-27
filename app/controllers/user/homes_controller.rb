d514123beffd4cb71e905e368902a16d12214639
masatonara <masato.nara83@gmail.com>
2021-05-27 02:53:24 +0000

    push

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,7 +1,7 @@
 class User::HomesController < ApplicationController
   def top
     @user = current_user
-    @randam = Liquor.order('RANDOM()').limit(15)
+    @random = Liquor.order('RANDOM()').limit(15)
   end
 
   def about
