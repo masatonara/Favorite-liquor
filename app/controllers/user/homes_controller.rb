7144868ad8d0a43e54bc375f0d27f983d50277c3
masatonara <masato.nara83@gmail.com>
2021-05-31 01:26:38 +0000

    text  chenge

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,7 +1,7 @@
 class User::HomesController < ApplicationController
   def top
     @user = current_user
-    @liquors = Liquor.order("RAND()").limit(5)
+    @liquors = Liquor.order(:id)
   end
 
   def about
